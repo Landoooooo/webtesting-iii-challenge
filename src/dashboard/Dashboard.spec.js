7b63739c6e3adba6986c505c83e918620a8aa1c6
Alando Appling <alandoappling@gmail.com>
2019-04-16 17:46:18 -0700

    Completed Dashboard Test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,31 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Dashboard from './Dashboard';
+import Display from '../display/Display';
+
+
+describe("<Dashboard />", () => {
+    it("should show the controls and display", () => {
+       const { getByText } = render(<Dashboard/>);
+
+       expect(getByText(/Unlocked/i)).toBeInTheDocument();
+       expect(getByText(/Open/i)).toBeInTheDocument();
+       expect(getByText(/Lock Gate/i)).toBeInTheDocument();
+       expect(getByText(/Close Gate/i)).toBeInTheDocument();
+    })
+})
+
+describe("<Display/>", () => {
+    it("should display that gate is closed if it is locked", () => {
+        const { getByText } = render(<Display closed={true}/>)
+        expect(getByText(/Closed/i)).toBeInTheDocument();
+    })
+
+    it("should display that gate is open if it is unlocked", () => {
+        const { getByText } = render(<Display closed={false}/>)
+        expect(getByText(/Open/i)).toBeInTheDocument();
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Display from './Display';
 // Test away!
+describe("<Display/>", () => {
+    it("should display that gate is closed if it is locked", () => {
+        const { getByText } = render(<Display closed={true}/>)
+        expect(getByText(/Closed/i)).toBeInTheDocument();
+    })
+
+    it("should display that gate is open if it is unlocked", () => {
+        const { getByText } = render(<Display closed={false}/>)
+        expect(getByText(/Open/i)).toBeInTheDocument();
+    })
+})
