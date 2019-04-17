21b0e96632a53b1cd2c9130351ab584135ca6e0b
Alando Appling <alandoappling@gmail.com>
2019-04-16 20:23:38 -0700

    Completed Controls.js Test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,7 +4,6 @@ import { render, fireEvent } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
 import Dashboard from './Dashboard';
-import Display from '../display/Display';
 
 
 describe("<Dashboard />", () => {
@@ -15,17 +14,5 @@ describe("<Dashboard />", () => {
        expect(getByText(/Open/i)).toBeInTheDocument();
        expect(getByText(/Lock Gate/i)).toBeInTheDocument();
        expect(getByText(/Close Gate/i)).toBeInTheDocument();
-    })
-})
-
-describe("<Display/>", () => {
-    it("should display that gate is closed if it is locked", () => {
-        const { getByText } = render(<Display closed={true}/>)
-        expect(getByText(/Closed/i)).toBeInTheDocument();
-    })
-
-    it("should display that gate is open if it is unlocked", () => {
-        const { getByText } = render(<Display closed={false}/>)
-        expect(getByText(/Open/i)).toBeInTheDocument();
     })
 })
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
+import { render, fireEvent, cleanup } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
 import Display from './Display';
+
+afterEach(cleanup)
 // Test away!
 describe("<Display/>", () => {
     it("should display that gate is closed if it is locked", () => {
@@ -13,5 +15,15 @@ describe("<Display/>", () => {
     it("should display that gate is open if it is unlocked", () => {
         const { getByText } = render(<Display closed={false}/>)
         expect(getByText(/Open/i)).toBeInTheDocument();
+    })
+
+    it("should default to the red-led class when locked/close", () => {
+        const { getByText } = render(<Display closed={true} locked={true}/>)
+        expect(getByText(/locked/i)).toHaveClass('led red-led')
+    })
+
+    it("should default to the green-led class when unlocked/open", () => {
+        const { getByText } = render(<Display closed={false} locked={false}/>);
+        expect(getByText(/open/i)).toHaveClass('led green-led')
     })
 })
